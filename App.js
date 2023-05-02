a29bf5efaf024fe43c863d0a6df546f1a26e1529
Mbilal6799 <ftk4321@gmail.com>
2023-05-02 13:31:52 -0700

    BE-3 if there is no user in database in db:create one IN APP.JS Screen

--- App.js
+++ App.js
@@ -3,16 +3,52 @@ import { StyleSheet, Text, View, StatusBar } from "react-native";
 import Navigator from "./src/Navigation";
 
 //step 1 import amplify
-import { Amplify } from "aws-amplify";
+import { Amplify, Auth, API, graphqlOperation } from "aws-amplify";
 import amplifyConfig from "./src/aws-exports";
 
 //step2 For Signup and SignIn Page Import withAuthenticator
 import { withAuthenticator } from "aws-amplify-react-native";
-
+import { useEffect } from "react";
+import { getUser } from "./src/graphql/queries";
+import { createUser } from "./src/graphql/mutations";
 //step 1 first step initilize amplify
 Amplify.configure({ ...amplifyConfig, Analytics: { disabled: true } });
 
 function App() {
+  useEffect(() => {
+    const syncData = async () => {
+      //Get User Auth Data
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      });
+      console.log("first", authUser);
+
+      //query the database using Auth user id (sub)
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes.sub })
+      );
+      console.log("second", userData.data.getUser);
+
+      if (userData.data.getUser) {
+        console.log("User Already Exists in Db");
+        return;
+      }
+
+      //if there is no user in database in db:create one
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        status: "Hey There! I am using WhatsApp.",
+      };
+
+      const newUserResponse = await API.graphql(
+        graphqlOperation(createUser, { input: newUser })
+      );
+    };
+
+    syncData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Navigator />
